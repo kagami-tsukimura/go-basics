19047bbe8e8d2ff3c8547348c8915bb5497db965
Kagami <shimura.kyohei7617@gmail.com>
2024-05-18 01:38:07 +0900

    auto: auto command push

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 	sl := []int{1, 2, 3}
-	if len(sl) < 0 {
-		fmt.Println("unreachable code...")
+	if len(sl) > 1 {
+		fmt.Println("multiple list!")
 	}
 }
