9835d5319939338666f7f4e2a1b1ce6b05fb9316
Kagami <shimura.kyohei7617@gmail.com>
2024-05-18 10:43:34 +0900

    auto: auto command push

--- main.go
+++ main.go
@@ -12,5 +12,8 @@ func main() {
 	godotenv.Load()
 	fmt.Println(os.Getenv("GO_ENV"))
 	fmt.Println(calculator.Offset)
-	fmt.Println(calculator.Sum(1, 2))
+
+	sumResult := fmt.Sprintf("a + b + offset = %f", calculator.Sum(1, 2))
+
+	fmt.Println(sumResult)
 }
