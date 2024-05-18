2dafd3867f9802d7dcf37c40050896b408e1c0e4
Kagami <shimura.kyohei7617@gmail.com>
2024-05-18 10:51:22 +0900

    auto: auto command push

--- main.go
+++ main.go
@@ -13,7 +13,8 @@ func main() {
 	fmt.Println(os.Getenv("GO_ENV"))
 	fmt.Println(calculator.Offset)
 
-	sumResult := calculator.Sum(1, 2.1)
-	fmt.Printf("a + b + offset = %g\n", sumResult)
-
+	sumResultNumber := calculator.Sum(1, 2)
+	sumResultFloat := calculator.Sum(1, 2.1)
+	fmt.Printf("a + b + offset = %g\n", sumResultNumber)
+	fmt.Printf("a + b + offset = %g\n", sumResultFloat)
 }
