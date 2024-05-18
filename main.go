bf1d76bf0881c928dd270035e5daace9348dc46a
Kagami <shimura.kyohei7617@gmail.com>
2024-05-18 22:41:29 +0900

    auto: auto command push

--- main.go
+++ main.go
@@ -280,7 +280,7 @@ func slices() {
 	fs6[1] = 7
 	fs6 = append(fs6, 8)
 	// s5[1]: 6, s5[2]: 7,
-	// s5[3]: 0 (メモリ共有はインデックス2までのため、共有されない)
+	// s5[3]: 0 (メモリ共有はインデックス2までのため、appendは共有されない)
 	fmt.Printf("s5_type: %[1]T, value: %[1]v, len: %v, cap: %v\n", s5, len(s5), cap(s5))
 	fmt.Printf("fs6_type: %[1]T, value: %[1]v, len: %v, cap: %v\n", fs6, len(fs6), cap(fs6))
 	fmt.Println("----------")
