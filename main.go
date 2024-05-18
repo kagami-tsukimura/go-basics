c1bc0d448feed70b86b1c8be919a3e7e5ab3fcbe
Kagami <shimura.kyohei7617@gmail.com>
2024-05-19 08:04:14 +0900

    auto: auto command push

--- main.go
+++ main.go
@@ -402,9 +402,9 @@ func (task *Task) extendEstimateByPointer() {
 func functions() {
 	// defer: 関数終了時に遅れて実行
 	// deferが複数: 下から上に実行
-	defer fmt.Println("defer func finally") // 3
-	defer fmt.Println("defer func 1")       // 2
-	fmt.Println("Hello World")              // 1
+	defer fmt.Println("defer func finally")   // 3
+	defer fmt.Println("defer func seminally") // 2
+	fmt.Println("Hello World")                // 1
 }
 
 func main() {
