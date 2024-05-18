fa2004a3726168f25237922bcf5022a048158378
Kagami <shimura.kyohei7617@gmail.com>
2024-05-18 23:20:12 +0900

    auto: auto command push

--- main.go
+++ main.go
@@ -351,11 +351,13 @@ func structs() {
 	fmt.Printf("task1: %v, task2: %v\n", task1.Title, task2.Title)
 	fmt.Println("----------")
 
+	// pointerアドレスの格納
 	task1p := &Task{
 		Title:    "Learning concurrency",
 		Estimate: 2,
 	}
 	fmt.Printf("task1p: %v\n", task1p)
+	fmt.Printf("task1p: %v\n", *task1p)
 }
 
 func main() {
