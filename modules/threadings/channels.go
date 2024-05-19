1efd3d2bc0712b2844fb37c9f38fc0fac5acd6d0
Kagami <shimura.kyohei7617@gmail.com>
2024-05-19 21:13:37 +0900

    auto: auto command push

--- modules/threadings/channels.go
+++ modules/threadings/channels.go
@@ -1,8 +1,22 @@
 package threadings
 
+import (
+	"fmt"
+	"sync"
+)
+
 func Channels() {
 	// // deadlock
 	// ch := make(chan int)
 	// ch <- 10
 	// fmt.Println(<-ch)
+	ch := make(chan int)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fmt.Println(<-ch)
+	}()
+	ch <- 10
+	wg.Wait()
 }
