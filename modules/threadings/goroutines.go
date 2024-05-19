879a6aaf80a8e464f88bffe8ec6025b3ea13f950
Kagami <shimura.kyohei7617@gmail.com>
2024-05-19 19:03:54 +0900

    auto: auto command push

--- modules/threadings/goroutines.go
+++ modules/threadings/goroutines.go
@@ -3,16 +3,20 @@ package threadings
 import (
 	"fmt"
 	"runtime"
+	"sync"
 )
 
 func Goroutines() {
-	fmt.Println("Goroutines")
+	var wg sync.WaitGroup
+	wg.Add(1)
 	// 無名関数の先頭に「go」: goroutinesとして起動
 	go func() {
+		defer wg.Done()
 		fmt.Println("Goroutines")
 	}()
+	// goroutinesの終了を待機
+	wg.Wait()
 	// メインと独立でgoroutinesは起動し、起動に時間がかかる。
-	// mainのgoroutine, go func()のgoroutine
 	fmt.Printf("num of working goroutines: %d\n", runtime.NumGoroutine())
 	fmt.Println("main func finished")
 }
