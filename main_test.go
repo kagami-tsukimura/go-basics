b6bc1d20b267d09d35acf05a52c89a1259f3d6c2
Kagami <shimura.kyohei7617@gmail.com>
2024-05-19 21:21:49 +0900

    auto: auto command push

--- main_test.go
+++ main_test.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"go-basics/modules/threadings"
 	"testing"
 
 	"go.uber.org/goleak"
 )
 
 func TestLeak(t *testing.T) {
+	// goroutine leakのテスト
 	defer goleak.VerifyNone(t)
-	main()
+	threadings.Channels()
 }
