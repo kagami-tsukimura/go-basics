a5691999dc8a4c9a36f705430bedb10467341b3a
Kagami <shimura.kyohei7617@gmail.com>
2024-05-19 13:25:57 +0900

    auto: auto command push

--- modules/errors.go
+++ modules/errors.go
@@ -8,6 +8,16 @@ import (
 func Errors() {
 
 	err01 := errors.New("something wrong")
-	fmt.Printf("%[1]p %[1]T %[1]v\n", err01)
+	fmt.Printf("pointer: %[1]p\ntype: %[1]T\nvalue: %[1]v\n", err01)
+	fmt.Println(err01.Error())
+	fmt.Println(err01)
 	fmt.Println("----------")
+	err02 := errors.New("something wrong")
+	fmt.Printf("pointer: %[1]p\ntype: %[1]T\nvalue: %[1]v\n", err01)
+	fmt.Printf("err01: %[1]p\nerr02: %[1]p\n", err01, err02)
+	fmt.Println(err01 == err02)
+
+	err0 := fmt.Errorf("add info: %w", errors.New("origin error"))
+	fmt.Printf("pointer: %[1]p\ntype: %[1]T\nvalue: %[1]v\n", err0)
+
 }
