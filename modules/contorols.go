6ee5329eb218336547bda6e29e0cc47a499909e0
Kagami <shimura.kyohei7617@gmail.com>
2024-05-19 12:00:47 +0900

    auto: auto command push

--- modules/contorols.go
+++ modules/contorols.go
@@ -3,7 +3,15 @@ package modules
 import "fmt"
 
 func Controls() {
-	fmt.Println("----------")
-	fmt.Println("Controls")
+	a := -1
+
+	if a == 0 {
+		fmt.Println("zero")
+	} else if a > 0 {
+		fmt.Println("positive")
+	} else {
+		fmt.Println("negative")
+	}
+
 	fmt.Println("----------")
 }
