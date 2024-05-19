c9ef300803dc675d9b87d6624533412674befb6f
Kagami <shimura.kyohei7617@gmail.com>
2024-05-19 13:59:36 +0900

    auto: auto command push

--- modules/generics.go
+++ modules/generics.go
@@ -2,7 +2,19 @@ package modules
 
 import "fmt"
 
+type customConstraints interface {
+	int | int16 | float32 | float64 | string
+}
+
+func add[T customConstraints](x, y T) T {
+	return x + y
+}
+
 func Generics() {
-	fmt.Println("generics")
+	fmt.Printf("%v\n", add(1, 2))
+	fmt.Printf("%v\n", add(1.1, 2.1))
+	fmt.Printf("%v\n", add("file", ".txt"))
+	// bool型はcustomConstraintsに定義されていないためエラー
+	// fmt.Printf("%v\n", add(true, false))
 	fmt.Println("----------")
 }
