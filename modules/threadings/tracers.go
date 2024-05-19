87b5eaaa755083eadeba7f670dda430484f0b339
Kagami <shimura.kyohei7617@gmail.com>
2024-05-19 19:12:34 +0900

    auto: auto command push

--- modules/threadings/tracers.go
+++ modules/threadings/tracers.go
@@ -1,7 +1,20 @@
 package threadings
 
-import "fmt"
+import (
+	"log"
+	"os"
+)
 
 func Tracers() {
-	fmt.Println("Tracers")
+	f, err := os.Create("trace.out")
+	if err != nil {
+		log.Fatalln("Error: ", err)
+	}
+
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Fatalln("Error: ", err)
+		}
+	}()
+
 }
