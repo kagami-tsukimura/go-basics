e897680b3d9b8d4dd6b76639222da6fc1439750f
Kagami <shimura.kyohei7617@gmail.com>
2024-05-19 14:59:01 +0900

    auto: auto command push

--- tests/unitTest.go
+++ tests/unitTest.go
@@ -2,6 +2,18 @@ package tests
 
 import "fmt"
 
+func Add(x, y int) int {
+	return x + y
+}
+
+func Devide(x, y int) float32 {
+	if y == 0 {
+		return 0.
+	}
+	return float32(x) / float32(y)
+}
+
 func UnitTest() {
-	fmt.Println("unitTest")
+	fmt.Println(Add(1, 2))
+	fmt.Println(Devide(1, 2))
 }
