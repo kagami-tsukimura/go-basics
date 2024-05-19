30e1e15437c6fe9f9cb383012b1dbf7c2fee6d2e
Kagami <shimura.kyohei7617@gmail.com>
2024-05-19 15:15:17 +0900

    auto: auto command push

--- modules/logs.go
+++ modules/logs.go
@@ -1,7 +1,30 @@
 package modules
 
-import "fmt"
+import (
+	"log"
+	"os"
+)
+
+func mkDir(dir string) {
+	_, err := os.Stat(dir)
+	if err != nil {
+		// ディレクトリの存在をチェック
+		if os.IsNotExist(err) {
+			// 存在しないので作る
+			err = os.Mkdir(dir, 0755)
+			if err != nil {
+				// ディレクトリの作成に失敗
+				panic(err)
+			}
+		}
+	}
+}
 
 func Logs() {
-	fmt.Println("Hello Log")
+	mkDir("logger")
+	file, err := os.Create("logger/log.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
 }
