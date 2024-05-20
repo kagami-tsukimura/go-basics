35b6a4f68a7643bbd664a75ab02dd110df46c1aa
Kagami <shimura.kyohei7617@gmail.com>
2024-05-20 21:29:56 +0900

    auto: auto command push

--- modules/threadings/contexts.go
+++ modules/threadings/contexts.go
@@ -3,15 +3,32 @@ package threadings
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 )
 
+func subTask(ctx context.Context, wg *sync.WaitGroup, id string) {
+	defer wg.Done()
+	t := time.NewTicker(500 * time.Millisecond)
+	select {
+	case <-ctx.Done():
+		fmt.Println(ctx.Err())
+		return
+	case <-t.C:
+		t.Stop()
+		fmt.Println(id)
+	}
+}
+
 func Contexts() {
 	// Context: メインgoroutineからサブgoroutineを一括キャンセル
-	ctx, cancel := context.WithCancel(context.Background())
-	go func() {
-		time.Sleep(1 * time.Second)
-		cancel()
-	}()
-	fmt.Println(ctx.Err())
+	var wg sync.WaitGroup
+	ctx, cancel := context.WithTimeout(context.Background(), 600*time.Millisecond)
+	defer cancel()
+
+	wg.Add(3)
+	go subTask(ctx, &wg, "A")
+	go subTask(ctx, &wg, "B")
+	go subTask(ctx, &wg, "C")
+	wg.Wait()
 }
