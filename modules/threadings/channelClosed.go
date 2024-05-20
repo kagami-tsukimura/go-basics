bb7a986f90126ce25181086066a00e1ea3a97c76
Kagami <shimura.kyohei7617@gmail.com>
2024-05-20 19:24:17 +0900

    auto: auto command push

---
+++ modules/threadings/channelClosed.go
@@ -0,0 +1,23 @@
+package threadings
+
+import (
+	"fmt"
+	"sync"
+)
+
+func ChannelClosed() {
+	ch1 := make(chan int)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fmt.Println(<-ch1)
+	}()
+	ch1 <- 10
+	close(ch1)
+
+	v, ok := <-ch1
+	fmt.Println(v, ok)
+	wg.Wait()
+}
