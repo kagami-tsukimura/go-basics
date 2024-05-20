1ac182eb9d6d1433dc939eb942b56bb99e1db15d
Kagami <shimura.kyohei7617@gmail.com>
2024-05-20 21:05:36 +0900

    auto: auto command push

--- modules/threadings/mutexes.go
+++ modules/threadings/mutexes.go
@@ -3,6 +3,7 @@ package threadings
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 func raceMutexes() {
@@ -33,6 +34,25 @@ func raceMutexes() {
 	fmt.Println("----------")
 }
 
+func rwMutexes() {
+	var wg sync.WaitGroup
+	var mu sync.RWMutex
+	var c int
+	read(&mu, &wg, &c)
+}
+
+func read(mu *sync.RWMutex, wg *sync.WaitGroup, c *int) {
+	defer wg.Done()
+	time.Sleep(10 * time.Millisecond)
+	mu.RLock()
+	defer mu.RUnlock()
+	fmt.Println("read lock")
+	fmt.Println("c: ", *c)
+	time.Sleep(10 * time.Second)
+	fmt.Println("read unlock")
+}
+
 func Mutexes() {
 	raceMutexes()
+	rwMutexes()
 }
