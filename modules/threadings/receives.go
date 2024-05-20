b83e387f018c28417a37fe64040e09872338b0c5
Kagami <shimura.kyohei7617@gmail.com>
2024-05-20 20:49:56 +0900

    auto: auto command push

--- modules/threadings/receives.go
+++ modules/threadings/receives.go
@@ -13,7 +13,7 @@ func Packages() {
 	ch1 := make(chan int, bufferSize)
 	ch2 := make(chan int, bufferSize)
 	var wg sync.WaitGroup
-	ctx, cancel := context.WithTimeout(context.Background(), 3000*time.Millisecond)
+	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
 	defer cancel()
 	wg.Add(3)
 	go countProducer(&wg, ch1, bufferSize, 50)
