b0796d38996d6b688bed1ed42c3c5fe023c9b303
Kagami <shimura.kyohei7617@gmail.com>
2024-05-20 19:41:14 +0900

    auto: auto command push

--- modules/threadings/selects.go
+++ modules/threadings/selects.go
@@ -1,7 +1,23 @@
 package threadings
 
-import "fmt"
+import (
+	"sync"
+	"time"
+)
 
 func Selects() {
-	fmt.Println("Selects")
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		time.Sleep(500 * time.Millisecond)
+		ch1 <- "A"
+	}()
+	go func() {
+		defer wg.Done()
+		time.Sleep(800 * time.Millisecond)
+		ch2 <- "B"
+	}()
 }
