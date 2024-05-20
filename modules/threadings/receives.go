a9de8bfa4da98c7b3c413b8d56118f16781af13f
Kagami <shimura.kyohei7617@gmail.com>
2024-05-20 20:41:44 +0900

    auto: auto command push

--- modules/threadings/receives.go
+++ modules/threadings/receives.go
@@ -1,7 +1,21 @@
 package threadings
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 func Packages() {
 	fmt.Println("Packages")
+}
+
+func countProductor(wg *sync.WaitGroup, ch chan<- int, size int, sleep int) {
+	defer wg.Done()
+	defer close(ch)
+
+	for i := 0; i < size; i++ {
+		time.Sleep(time.Duration(sleep) * time.Millisecond)
+		ch <- i
+	}
 }
