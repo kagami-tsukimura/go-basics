2b84e789f9f6cac5b29995ae0095be53ed015a0c
Kagami <shimura.kyohei7617@gmail.com>
2024-05-20 20:45:23 +0900

    auto: auto command push

--- modules/threadings/receives.go
+++ modules/threadings/receives.go
@@ -1,6 +1,7 @@
 package threadings
 
 import (
+	"context"
 	"fmt"
 	"sync"
 	"time"
@@ -17,5 +18,29 @@ func countProducer(wg *sync.WaitGroup, ch chan<- int, size int, sleep int) {
 	for i := 0; i < size; i++ {
 		time.Sleep(time.Duration(sleep) * time.Millisecond)
 		ch <- i
+	}
+}
+
+func countConsumer(ctx context.Context, wg *sync.WaitGroup, ch1 <-chan int, ch2 chan<- int) {
+	defer wg.Done()
+loop:
+	for ch1 != nil || ch2 != nil {
+		select {
+		case <-ctx.Done():
+			fmt.Println(ctx.Err())
+			break loop
+		case v, ok := <-ch1:
+			if !ok {
+				ch1 = nil
+				break
+			}
+			fmt.Printf("ch1 %v\n", v)
+		case v, ok := <-ch2:
+			if !ok {
+				ch2 = nil
+				break
+			}
+			fmt.Printf("ch2 %v\n", v)
+		}
 	}
 }
