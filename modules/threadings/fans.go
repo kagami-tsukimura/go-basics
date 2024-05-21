6142f0f6f459e27881d657a667312a6430b9e205
Kagami <shimura.kyohei7617@gmail.com>
2024-05-21 20:16:15 +0900

    auto: auto command push

--- modules/threadings/fans.go
+++ modules/threadings/fans.go
@@ -1,6 +1,18 @@
 package threadings
 
+import (
+	"fmt"
+	"runtime"
+)
+
 func Fans() {
 	// Fan-out: Pipeline内のchannel値を複数のgoroutineに分散
 	// Fan-in: 複数の出力channelをマージ
+
+	cores := runtime.NumCPU()
+	fmt.Println(cores)
+	// ctx, cancel := context.WithCancel(context.Background())
+	// defer cancel()
+	// nums := []int{1, 2, 3, 4, 5}
+	// var i int
 }
