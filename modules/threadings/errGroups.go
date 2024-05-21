1fd8607529f5324306fe86b09cf75ea73bb7077d
Kagami <shimura.kyohei7617@gmail.com>
2024-05-21 19:26:50 +0900

    auto: auto command push

--- modules/threadings/errGroups.go
+++ modules/threadings/errGroups.go
@@ -1,27 +1,47 @@
 package threadings
 
 import (
+	"context"
 	"fmt"
+	"time"
 
 	"golang.org/x/sync/errgroup"
 )
 
-func doTask(task string) error {
-	if task == "fake1" || task == "fake2" {
-		return fmt.Errorf("%v failed", task)
+func doTask(ctx context.Context, task string) error {
+	var t *time.Ticker
+	switch task {
+	case "fake1":
+		t = time.NewTicker(500 * time.Millisecond)
+	case "fake2":
+		t = time.NewTicker(700 * time.Millisecond)
+	default:
+		t = time.NewTicker(1000 * time.Millisecond)
 	}
-	fmt.Printf("task %v done\n", task)
+
+	select {
+	case <-ctx.Done():
+		fmt.Printf("%v cancelled: %v\n", task, ctx.Err())
+		return ctx.Err()
+	case <-t.C:
+		t.Stop()
+		if task == "fake1" || task == "fake2" {
+			return fmt.Errorf("%v process failed", task)
+		}
+		fmt.Printf("%v done\n", task)
+	}
 	return nil
 }
 
 func ErrGroups() {
-	eg := new(errgroup.Group)
-	s := []string{"task1", "fake1", "task2", "fake2"}
+	// 初回エラー時にキャンセル
+	eg, ctx := errgroup.WithContext(context.Background())
+	s := []string{"task1", "fake1", "task2", "fake2", "task3"}
 
 	for _, v := range s {
 		task := v
 		eg.Go(func() error {
-			return doTask(task)
+			return doTask(ctx, task)
 		})
 	}
 
