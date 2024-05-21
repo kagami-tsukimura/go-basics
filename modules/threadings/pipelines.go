7dbed85a9f1018ec9c65c62e0c9e1e746e3282a3
Kagami <shimura.kyohei7617@gmail.com>
2024-05-21 19:40:49 +0900

    auto: auto command push

--- modules/threadings/pipelines.go
+++ modules/threadings/pipelines.go
@@ -1,9 +1,29 @@
 package threadings
 
-import "fmt"
+import (
+	"context"
+)
+
+func generator(ctx context.Context, nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for _, n := range nums {
+			select {
+			case <-ctx.Done():
+				return
+			case out <- n:
+			}
+		}
+	}()
+	return out
+}
 
 func Pipelines() {
 	// Pipeline: 各処理をステージ上に配置
 	// channel送受信でプロセスを流す
-	fmt.Println("Pipeline")
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	nums := []int{1, 2, 3, 4, 5}
+	generator(ctx, nums...)
 }
