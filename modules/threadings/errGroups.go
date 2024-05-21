8fc34038785456d7508e913a2461505f22eb94fd
Kagami <shimura.kyohei7617@gmail.com>
2024-05-21 19:18:32 +0900

    auto: auto command push

--- modules/threadings/errGroups.go
+++ modules/threadings/errGroups.go
@@ -1,7 +1,20 @@
 package threadings
 
-import "fmt"
+import (
+	"fmt"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func doTask(task string) error {
+	if task == "fake1" || task == "fake2" {
+		return fmt.Errorf("%v failed", task)
+	}
+	fmt.Printf("task %v done\n", task)
+	return nil
+}
 
 func ErrGroups() {
-	fmt.Println("ErrGroups")
+	eg := new(errgroup.Group)
+	s := []string{"task1", "fake1", "task2", "fake2"}
 }
